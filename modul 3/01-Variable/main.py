59aaa4cd1e9a3553dd7409490cf8428a7c3e4fca
WULANNCHH <wulanaulia2024@gmail.com>
2024-11-11 10:03:03 +0800

    Add files via upload

---
+++ modul 3/01-Variable/main.py
@@ -0,0 +1,26 @@
+# Varible adalah wadah yang menampung nilai / data
+
+# cara menaruh nilai / assigment data
+r = 10
+f = 130
+Luas = 1/2
+
+# memangil Varible
+print("nilai r = ", r)
+print("nilai f = ", f)
+print("nilai luas = ", Luas)
+
+# pernamaan varible yang benar 
+circle_area = 314.16 # dibulatkan ke dua angka desimal
+celsius_temperature = 54.44 # menggunakan rumus desimal
+triangle_area = 25.0 ## Mengubah hasil ke integer
+
+# Menghitung luas lingkaran
+# Mengonversi suhu ke Celsius
+# Menghitung luas segitiga
+
+a = 7
+print("nilai a =", a)
+
+b = a
+print("nilai b =", b)
